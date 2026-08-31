02f43ddcbb75ca703193c9d55ba17e65c03972db
Mike <mike@example.com>
2026-08-31 15:16:26 -0600

    Fixed Gmail network connection

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ const pool = new Pool({
 
 // 👉 NEW: Setting up the secure login to your Gmail
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
